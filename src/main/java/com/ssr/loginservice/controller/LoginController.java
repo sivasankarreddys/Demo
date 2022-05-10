7a7c43af1c792612b468894ee412d287051fbb72
siva sankar reddy s <44916546+sivasankarreddys@users.noreply.github.com>
2022-05-10 21:01:51 +0530

    changes at version2

--- src/main/java/com/ssr/loginservice/controller/LoginController.java
+++ src/main/java/com/ssr/loginservice/controller/LoginController.java
@@ -1,21 +1,14 @@
 package com.ssr.loginservice.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssr.loginservice.beans.LoginBean;
 import com.ssr.loginservice.model.LoginModel;
 import com.ssr.loginservice.repository.LoginRepository;
-
-import antlr.collections.List;
 
 @RestController
 @RequestMapping("/api")
