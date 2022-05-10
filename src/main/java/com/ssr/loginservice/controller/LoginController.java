a0cb209d1e7e0fc6a90258e2accc67d9994df3fa
siva sankar reddy s <44916546+sivasankarreddys@users.noreply.github.com>
2022-05-10 19:38:15 +0530

    Revert "get the account details.."
    This reverts commit 472e48e02fceaaddcdf96ab102909fde1f39702e.

--- src/main/java/com/ssr/loginservice/controller/LoginController.java
+++ src/main/java/com/ssr/loginservice/controller/LoginController.java
@@ -1,58 +1,53 @@
 package com.ssr.loginservice.controller;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssr.loginservice.LoginService;
-import com.ssr.loginservice.model.Accounts;
+import com.ssr.loginservice.beans.LoginBean;
 import com.ssr.loginservice.model.LoginModel;
 import com.ssr.loginservice.repository.LoginRepository;
 
-
+import antlr.collections.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class LoginController {
-
+    
 	@Autowired
 	LoginRepository loginReposiory;
 
 	LoginModel loginModel = new LoginModel();
-	Accounts accounts = new Accounts();
-	@Autowired
-	private LoginService loginservice;
-
+	
+   
 	@PostMapping("/registration")
-	public ResponseEntity<LoginModel> registration(@RequestBody LoginModel loginbean) {
+	public String registration(@RequestBody LoginModel loginbean) {
+	
 
-		if (loginbean.getPassword().length() > 3) {
-
-			loginservice.saveProduct(loginbean);
-			return new ResponseEntity<LoginModel>(loginbean,HttpStatus.CREATED);
-		}
-
-		return new ResponseEntity<LoginModel>(loginbean, HttpStatus.LENGTH_REQUIRED);
-	}
-
-	@GetMapping("/accounts")
-	public ResponseEntity<List<Accounts>> fetchAccounts() {
+			System.out.println("Entery point");
+			
+			loginReposiory.save(loginbean);
 		
-		List<Accounts> accounts = loginservice.fetchAccounts();
-		if(accounts.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.CONFLICT);
-		}
-      
-		return new ResponseEntity<>(accounts,HttpStatus.OK);
+			return "sucess";
 	}
+	
+	@GetMapping("/login")
+	public boolean loginValidate(@RequestBody LoginModel loginbean) {
+		
+		if(loginbean != null) {
+			
+		//ArrayList<LoginModel> list  =loginReposiory.findOne(loginbean.getUsername());
+		}
+		
+		return true;
+	}
+	
 
 }
--- src/main/java/com/ssr/loginservice/repository/LoginRepository.java
+++ src/main/java/com/ssr/loginservice/repository/LoginRepository.java
@@ -2,6 +2,7 @@ package com.ssr.loginservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.ssr.loginservice.model.LoginModel;
 
 public interface LoginRepository extends JpaRepository<LoginModel, Long>{
