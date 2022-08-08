6189a99526f1377cab2f94928c0fd6e9940053b7
siva sankar reddy s <44916546+sivasankarreddys@users.noreply.github.com>
2022-08-08 16:27:05 +0530

    latest changes..

--- src/main/java/com/ssr/loginservice/controller/LoginController.java
+++ src/main/java/com/ssr/loginservice/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.ssr.loginservice.controller;
 
+import java.util.List;
+
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,38 +12,51 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssr.loginservice.model.LoginModel;
 import com.ssr.loginservice.repository.LoginRepository;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 
+@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class LoginController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);	   
     
+
 	@Autowired
 	LoginRepository loginReposiory;
+	
 
 	LoginModel loginModel = new LoginModel();
-	
-   
+
 	@PostMapping("/registration")
 	public String registration(@RequestBody LoginModel loginbean) {
 	
+		System.out.println("Entery point");
 
-			System.out.println("Entery point");
-			
-			loginReposiory.save(loginbean);
-		
-			return "sucess";
+		loginReposiory.save(loginbean);
+
+		return "sucess";
 	}
-	
-	@GetMapping("/login")
-	public boolean loginValidate(@RequestBody LoginModel loginbean) {
+
+	@PostMapping("/login")
+	public String loginValidate(@RequestBody LoginModel login) {      
+		logger.warn("before validate login use json login object");
+		if (login.getUsername() != "" && login.getPassword() != "") {
+			List<LoginModel> loginModel = loginReposiory.findByUsername(login.getUsername());
+			for (LoginModel result : loginModel)
+				if (result.getUsername().contains(login.getUsername())) {
+					//logger.log(Level.INFO, login.getUsername());
+					return "sucess--" + login.getUsername();
+				}
 		
-		if(loginbean != null) {
-			
-		//ArrayList<LoginModel> list  =loginReposiory.findOne(loginbean.getUsername());
+			logger.debug("we created new new user.");
+			return "we created new new user.";
 		}
-		
-		return true;
+
+		return "login failed";
 	}
 	
+
 
 }
--- src/main/java/com/ssr/loginservice/repository/LoginRepository.java
+++ src/main/java/com/ssr/loginservice/repository/LoginRepository.java
@@ -1,11 +1,18 @@
 package com.ssr.loginservice.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ssr.loginservice.model.LoginModel;
 
 public interface LoginRepository extends JpaRepository<LoginModel, Long>{
+	
+	
+	 @Query("select u from LoginModel u where u.username = ?1")
+	 List<LoginModel> findByUsername(String username);
 
 	
 }
